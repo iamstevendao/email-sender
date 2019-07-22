0550639abbcd60d0a1e6ae0a0ae70aec14409558
Steven Dao <iamstevendao@gmail.com>
2019-07-23 00:27:56 +1000

    Add layout for the home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,61 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+.row
+  .col-sm-12
+    h2.text-center Compose an Email
+
+  .col-sm-12.m-t-20
+    h3 from
+    .row
+      .offset-sm-2.col-sm-3
+        label(for="nameSender") Name
+        b-form-input(name="senderName", v-model="model.senderName", required, autofocus=true)
+      .col-sm-5
+        label(for="senderEmail") Email*
+        b-form-input(type="email", name="senderEmail", v-model="model.senderEmail", required)
+
+  .col-sm-12.m-t-20
+    h3 to
+    .row
+      .offset-sm-2.col-sm-3
+        label(for="recipientName") Name
+        b-form-input(name="recipientName", v-model="model.recipientName")
+      .col-sm-5
+        label(for="recipientEmail") Emails*
+        b-form-input(name="recipientEmail",
+                    placeholder="Use ; for multiple emails (no spaces)",
+                    v-model="model.recipientEmail")
+
+  .col-sm-12.m-t-20
+    h3 content
+    .row
+      .offset-sm-2.col-sm-8
+        label(for="subject") Subject*
+        b-form-input(name="subject", v-model="model.subject", required)
+      .offset-sm-2.col-sm-8
+        label(for="body") Body
+        b-form-textarea(name="body", v-model="model.body")
+
+  .col-sm-12.text-center.m-t-20
+    b-button.btn-primary(type="submit") Submit
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: 'Home',
+  data: () => ({
+    model: {
+      senderName: '',
+      senderEmail: '',
+      recipientName: '',
+      subject: '',
+      recipientEmail: '',
+      body: '',
+    },
+  }),
+  methods: {
+    submit() {
+
+    },
   },
 };
 </script>
