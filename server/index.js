b31c2a482b1aefa84294b726eaaf801da15f150a
Steven Dao <iamstevendao@gmail.com>
2019-07-23 18:26:15 +1000

    Handle requests and send emails

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+// Load environment variables from .env file
+dotenv.config();
+
+const sendService = require('./services');
 
 const app = express();
 
@@ -9,11 +14,17 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!'
-  });
-});
+app
+  .get('/', (req, res) => {
+    res
+      .status(400)
+      .json({
+        errors: [
+          { message: 'POST method allowed only' },
+        ],
+      })
+  })
+  .post('/', sendService)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
